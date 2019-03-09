c898f3bd32d727eb7cc9e036e2bc9686c93f4691
Yatan Vesh <yatan.vesh@gmail.com>
2019-03-09 21:23:25 +0530

    Added Navbar, BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'reactn';
+import NavBar from './components/NavBar/NavBar';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        minimun react electron boilerplate
-      </div>
+      <BrowserRouter>
+            <div className='app-container'>
+                <NavBar signOut={this.signOut}/>
+                <Switch>
+               </Switch>
+
+            </div>
+        </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Pacifico','Oswald','Anton','Righteous', 'Rubik:600'],
+    }
+});
 
 ReactDOM.render(
   <App />,
